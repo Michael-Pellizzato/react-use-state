ea3800c8c91174a027f60c8375a610888816ec83
Michael-Pellizzato <pellizzato.michael97@gmail.com>
2025-02-26 19:32:49 +0100

    esercizio finito

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,71 @@
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+  import { useState } from "react";
+  import languages from "../src/assets/data/languages"
+  import 'bootstrap/dist/css/bootstrap.min.css';
 
-  return (
-    <>
-      
-    </>
-  )
-}
+  function App() {
 
-export default App
+    const [selectBottom, setSelectBottom] = useState (null)
+
+    const renderSelectBottom = () => {
+      if(! selectBottom) return <h3>nessun linguaggio selezionato</h3>
+
+      const {title, description} = selectBottom
+      return(
+        <div className="card-body">
+          <h4 className="card-title">{title}</h4>
+          <p className="card-text">{description}</p>
+        </div>
+      )
+    } 
+
+    const renderBottom = () => 
+      languages.map((lenguage) => {
+
+        const {id, title} = lenguage
+        //const active = selectButtom === lenguage
+        return(                          
+          <button 
+          key={id}
+          className='btn btn-primary'
+          onClick={() => setSelectBottom(lenguage)}
+          >{title}
+          </button>
+        )
+      }) 
+    
+
+
+
+    return (
+      <>
+        <header>
+              <div className="container">
+                  <div className="row">
+                      <div className="col">
+                          <h1>Learn Web Development</h1>
+                      </div>
+                  </div>
+              </div>
+          </header> 
+          <main>
+          <div className="container">
+                  <div className="row">
+                      <div className="col d-flex gap-2">
+                        {renderBottom()}                          
+                      </div>
+                  </div>
+              </div>
+              <div className='container'>
+                <div className="card">
+                  {renderSelectBottom ()}
+                </div>
+              </div>
+              
+              
+          </main>
+      </>
+    )
+  }
+
+  export default App
